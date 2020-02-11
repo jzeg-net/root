1d8ffd7d1166ea293d63036fc9da89ceeab3d9ea
zhangchenglin <469946668@qq.com>
2020-02-11 21:30:57 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
+    <title>杰格网</title>
+    <meta name="description" content="致力于职工社区的娱乐生活。">
+</head>
+<body>
+<div class="container  text-center small">
+    <p>建设中</p>
+</div>
+<div class="container fixed-bottom text-center small">
+    <span class="d-block small">联系邮箱&nbsp;<a class="text-reset" title="联系邮箱" href="mailto:admin@jzeg.net">admin@jzeg.net</a></span>
+    <a class="small text-reset text-decoration-none" target="_blank" href="http://www.beian.miit.gov.cn/">冀ICP备12018851号-7</a>
+    <a class="small text-reset text-decoration-none" target="_blank"
+       href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13050002001901">冀公网安备&nbsp;13050002001901号</a>
+</div>
+<div class="d-none">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"></script>
+    <script>
+        $().ready(function () {
+            $('a').tooltip();
+        });
+    </script>
+</div>
+</body>
+</html>
