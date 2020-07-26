c409e15c08e20e5ff05a1744552cc09ce00ae4a1
zhangchenglin <469946668@qq.com>
2020-07-26 09:05:26 +0800

    Initial commit

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
     <title>杰格网</title>
     <meta name="description" content="致力于职工社区的娱乐生活。">
+    <?php require_once dirname(__FILE__) . '/googleAnalytics/gtag_js.php'; ?>
 </head>
 <body>
 <div class="container  text-center small">
@@ -22,9 +23,8 @@
        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13050002001901">冀公网安备&nbsp;13050002001901号</a>
 </div>
 <div class="d-none">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
     <script>
         $().ready(function () {
             $('a').tooltip();
